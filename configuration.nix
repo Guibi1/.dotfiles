1d27c5982adb0bdadc17d19b216c23eb73fd3a85
Laurent Stéphenne <laurent@guibi.ca>
2024-01-11 14:51:36 -0500

    Added eduroam config for network manager

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }:
+let
+    vars = import ./vars.nix;
+in
 {
     imports = [
       /etc/nixos/hardware-configuration.nix
@@ -30,7 +33,70 @@
     # Networking options
     networking = {
         hostName = "Hermes"; # Define your hostname.
-        networkmanager.enable = true;
+
+        networkmanager = {
+            enable = true;
+            ensureProfiles.profiles = {
+                eduroam = {
+                    connection = {
+                        id = "eduroam";
+                        uuid = "0b3b1367-b305-4885-932a-48100e1e37b5";
+                        type = "wifi";
+                    };
+                    wifi = {
+                        cloned-mac-address = "stable";
+                        mode = "infrastructure";
+                        ssid = "eduroam";
+                    };
+                    wifi-security = {
+                        key-mgmt = "wpa-eap";
+                    };
+                    "802-1x" = {
+                        anonymous-identity = "anonymous657357@usherbrooke.ca";
+                        ca-cert = "/home/guibi/.config/cat_installer/ca.pem";
+                        domain-suffix-match = "radius.usherbrooke.ca";
+                        eap = "peap";
+                        identity = vars.eduroam.identity or "";
+                        password = vars.eduroam.password or "";
+                        phase2-auth = "mschapv2";
+                    };
+                };
+
+                UdsS = {
+                    connection = {
+                        id = "UdsS";
+                        uuid = "eb0e0e9c-8aa0-4541-ae59-51aca3dea1b5";
+                        type = "vpn";
+                        autoconnect = "false";
+                    };
+                    vpn = {
+                        authtype = "password";
+                        autoconnect-flags = "0";
+                        certsigs-flags = "0";
+                        cookie-flags = "2";
+                        disable_udp = "no";
+                        enable_csd_trojan = "no";
+                        gateway = "rpv.usherbrooke.ca";
+                        gateway-flags = "2";
+                        gwcert-flags = "2";
+                        lasthost-flags = "0";
+                        pem_passphrase_fsid = "no";
+                        prevent_invalid_cert = "no";
+                        protocol = "anyconnect";
+                        resolve-flags = "2";
+                        stoken_source = "disabled";
+                        useragent = "AnyConnect Linux_64 4.10.07061";
+                        xmlconfig-flags = "0";
+                        service-type = "org.freedesktop.NetworkManager.openconnect";
+                    };
+                    vpn-secrets = {
+                        lasthost = "rpv.usherbrooke.ca";
+                        save_passwords = "yes";
+                        xmlconfig = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4NCjxBbnlDb25uZWN0UHJvZmlsZSB4bWxucz0iaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvZW5jb2RpbmcvIiB4bWxuczp4c2k9Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvWE1MU2NoZW1hLWluc3RhbmNlIiB4c2k6c2NoZW1hTG9jYXRpb249Imh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL2VuY29kaW5nLyBBbnlDb25uZWN0UHJvZmlsZS54c2QiPg0KCTxDbGllbnRJbml0aWFsaXphdGlvbj4NCgkJPFVzZVN0YXJ0QmVmb3JlTG9nb24gVXNlckNvbnRyb2xsYWJsZT0idHJ1ZSI+ZmFsc2U8L1VzZVN0YXJ0QmVmb3JlTG9nb24+DQoJCTxBdXRvbWF0aWNDZXJ0U2VsZWN0aW9uIFVzZXJDb250cm9sbGFibGU9InRydWUiPmZhbHNlPC9BdXRvbWF0aWNDZXJ0U2VsZWN0aW9uPg0KCQk8U2hvd1ByZUNvbm5lY3RNZXNzYWdlPmZhbHNlPC9TaG93UHJlQ29ubmVjdE1lc3NhZ2U+DQoJCTxDZXJ0aWZpY2F0ZVN0b3JlPkFsbDwvQ2VydGlmaWNhdGVTdG9yZT4NCgkJPENlcnRpZmljYXRlU3RvcmVNYWM+QWxsPC9DZXJ0aWZpY2F0ZVN0b3JlTWFjPg0KCQk8Q2VydGlmaWNhdGVTdG9yZUxpbnV4PkFsbDwvQ2VydGlmaWNhdGVTdG9yZUxpbnV4Pg0KCQk8Q2VydGlmaWNhdGVTdG9yZU92ZXJyaWRlPmZhbHNlPC9DZXJ0aWZpY2F0ZVN0b3JlT3ZlcnJpZGU+DQoJCTxQcm94eVNldHRpbmdzPk5hdGl2ZTwvUHJveHlTZXR0aW5ncz4NCgkJPEFsbG93TG9jYWxQcm94eUNvbm5lY3Rpb25zPnRydWU8L0FsbG93TG9jYWxQcm94eUNvbm5lY3Rpb25zPg0KCQk8QXV0aGVudGljYXRpb25UaW1lb3V0PjEyPC9BdXRoZW50aWNhdGlvblRpbWVvdXQ+DQoJCTxBdXRvQ29ubmVjdE9uU3RhcnQgVXNlckNvbnRyb2xsYWJsZT0idHJ1ZSI+ZmFsc2U8L0F1dG9Db25uZWN0T25TdGFydD4NCgkJPE1pbmltaXplT25Db25uZWN0IFVzZXJDb250cm9sbGFibGU9InRydWUiPnRydWU8L01pbmltaXplT25Db25uZWN0Pg0KCQk8TG9jYWxMYW5BY2Nlc3MgVXNlckNvbnRyb2xsYWJsZT0idHJ1ZSI+dHJ1ZTwvTG9jYWxMYW5BY2Nlc3M+DQoJCTxEaXNhYmxlQ2FwdGl2ZVBvcnRhbERldGVjdGlvbiBVc2VyQ29udHJvbGxhYmxlPSJ0cnVlIj5mYWxzZTwvRGlzYWJsZUNhcHRpdmVQb3J0YWxEZXRlY3Rpb24+DQoJCTxDbGVhclNtYXJ0Y2FyZFBpbiBVc2VyQ29udHJvbGxhYmxlPSJ0cnVlIj50cnVlPC9DbGVhclNtYXJ0Y2FyZFBpbj4NCgkJPElQUHJvdG9jb2xTdXBwb3J0PklQdjQ8L0lQUHJvdG9jb2xTdXBwb3J0Pg0KCQk8QXV0b1JlY29ubmVjdCBVc2VyQ29udHJvbGxhYmxlPSJmYWxzZSI+dHJ1ZQ0KCQkJPEF1dG9SZWNvbm5lY3RCZWhhdmlvciBVc2VyQ29udHJvbGxhYmxlPSJmYWxzZSI+UmVjb25uZWN0QWZ0ZXJSZXN1bWU8L0F1dG9SZWNvbm5lY3RCZWhhdmlvcj4NCgkJPC9BdXRvUmVjb25uZWN0Pg0KCQk8U3VzcGVuZE9uQ29ubmVjdGVkU3RhbmRieT5mYWxzZTwvU3VzcGVuZE9uQ29ubmVjdGVkU3RhbmRieT4NCgkJPEF1dG9VcGRhdGUgVXNlckNvbnRyb2xsYWJsZT0iZmFsc2UiPnRydWU8L0F1dG9VcGRhdGU+DQoJCTxSU0FTZWN1cklESW50ZWdyYXRpb24gVXNlckNvbnRyb2xsYWJsZT0iZmFsc2UiPkF1dG9tYXRpYzwvUlNBU2VjdXJJREludGVncmF0aW9uPg0KCQk8V2luZG93c0xvZ29uRW5mb3JjZW1lbnQ+U2luZ2xlTG9jYWxMb2dvbjwvV2luZG93c0xvZ29uRW5mb3JjZW1lbnQ+DQoJCTxMaW51eExvZ29uRW5mb3JjZW1lbnQ+U2luZ2xlTG9jYWxMb2dvbjwvTGludXhMb2dvbkVuZm9yY2VtZW50Pg0KCQk8V2luZG93c1ZQTkVzdGFibGlzaG1lbnQ+TG9jYWxVc2Vyc09ubHk8L1dpbmRvd3NWUE5Fc3RhYmxpc2htZW50Pg0KCQk8TGludXhWUE5Fc3RhYmxpc2htZW50PkxvY2FsVXNlcnNPbmx5PC9MaW51eFZQTkVzdGFibGlzaG1lbnQ+DQoJCTxBdXRvbWF0aWNWUE5Qb2xpY3k+ZmFsc2U8L0F1dG9tYXRpY1ZQTlBvbGljeT4NCgkJPFBQUEV4Y2x1c2lvbiBVc2VyQ29udHJvbGxhYmxlPSJmYWxzZSI+RGlzYWJsZQ0KCQkJPFBQUEV4Y2x1c2lvblNlcnZlcklQIFVzZXJDb250cm9sbGFibGU9ImZhbHNlIj48L1BQUEV4Y2x1c2lvblNlcnZlcklQPg0KCQk8L1BQUEV4Y2x1c2lvbj4NCgkJPEVuYWJsZVNjcmlwdGluZyBVc2VyQ29udHJvbGxhYmxlPSJmYWxzZSI+dHJ1ZQ0KCQkJPFRlcm1pbmF0ZVNjcmlwdE9uTmV4dEV2ZW50PmZhbHNlPC9UZXJtaW5hdGVTY3JpcHRPbk5leHRFdmVudD4NCgkJCTxFbmFibGVQb3N0U0JMT25Db25uZWN0U2NyaXB0PnRydWU8L0VuYWJsZVBvc3RTQkxPbkNvbm5lY3RTY3JpcHQ+DQoJCTwvRW5hYmxlU2NyaXB0aW5nPg0KCQk8RW5hYmxlQXV0b21hdGljU2VydmVyU2VsZWN0aW9uIFVzZXJDb250cm9sbGFibGU9ImZhbHNlIj5mYWxzZQ0KCQkJPEF1dG9TZXJ2ZXJTZWxlY3Rpb25JbXByb3ZlbWVudD4yMDwvQXV0b1NlcnZlclNlbGVjdGlvbkltcHJvdmVtZW50Pg0KCQkJPEF1dG9TZXJ2ZXJTZWxlY3Rpb25TdXNwZW5kVGltZT40PC9BdXRvU2VydmVyU2VsZWN0aW9uU3VzcGVuZFRpbWU+DQoJCTwvRW5hYmxlQXV0b21hdGljU2VydmVyU2VsZWN0aW9uPg0KCQk8UmV0YWluVnBuT25Mb2dvZmY+ZmFsc2UNCgkJPC9SZXRhaW5WcG5PbkxvZ29mZj4NCgkJPENhcHRpdmVQb3J0YWxSZW1lZGlhdGlvbkJyb3dzZXJGYWlsb3Zlcj5mYWxzZTwvQ2FwdGl2ZVBvcnRhbFJlbWVkaWF0aW9uQnJvd3NlckZhaWxvdmVyPg0KCQk8QWxsb3dNYW51YWxIb3N0SW5wdXQ+dHJ1ZTwvQWxsb3dNYW51YWxIb3N0SW5wdXQ+DQoJPC9DbGllbnRJbml0aWFsaXphdGlvbj4NCgk8U2VydmVyTGlzdD4NCgkJPEhvc3RFbnRyeT4NCgkJCTxIb3N0TmFtZT5ycHYudXNoZXJicm9va2UuY2E8L0hvc3ROYW1lPg0KCQk8L0hvc3RFbnRyeT4NCgk8L1NlcnZlckxpc3Q+DQo8L0FueUNvbm5lY3RQcm9maWxlPg0K";
+                    };
+                };
+            };
+        };
     };
 
 
---
+++ vars.exemple.nix
@@ -0,0 +1,15 @@
+{
+    ### HOME MANAGER ###
+
+    # Id of the GPG key to use for git signing
+    git.gpgKey = "";
+
+
+    ### NIX OS ###
+
+    # The credentials to use for the eduroam wifi
+    eduroam = {
+        identity = "";
+        password = "";
+    };
+}
