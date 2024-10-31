958693da0baf8f602a2846b20c88cebddfa5ef9c
Laurent Stéphenne <laurent@guibi.dev>
2024-10-31 10:17:27 -0400

    Added MacOS support!

---
+++ darwin-config.nix
@@ -0,0 +1,51 @@
+{ config, lib, pkgs, ... }:
+let
+    vars = import ./vars.nix;
+in
+{
+    imports = [
+        <home-manager/nix-darwin>
+    ];
+
+
+    # Global packages
+    environment.systemPackages = with pkgs; [
+        git
+    ];
+
+
+    # Programs options
+    programs = {
+        zsh.enable = true;
+        fish.enable = true;
+    };
+
+
+    # Users options
+    users = {
+        users.guibi = {
+            shell = "${pkgs.fish}/bin/fish";
+            packages = with pkgs; [];
+        };
+    };
+    home-manager.users.guibi = import ./home.nix;
+
+
+    # Security options
+    security = {
+        pam.enableSudoTouchIdAuth = true;
+    };
+
+
+    # Custom configuration location
+    environment.darwinConfig = "$HOME/nix-config/darwin-config.nix";
+
+
+    # Auto upgrade nix package and the daemon service.
+    services.nix-daemon.enable = true;
+    nix.package = pkgs.nix;
+
+
+    # No touchy
+    system.stateVersion = 5;
+}
