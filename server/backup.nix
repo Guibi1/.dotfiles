a5c6846ea9cd624283568e2b8859ec777ae0db6d
Laurent Stéphenne <laurent@guibi.dev>
2025-05-14 16:55:17 -0400

    Azom borg backup

--- server/backup.nix
+++ server/backup.nix
@@ -6,9 +6,32 @@ in
     # Services options
     services = {
         borgbackup.jobs = {
-            myDailyBackup = {
-                description = "My daily file backup";
-                repo = "niftic.ssh:/path/to/your/remote/repo";
+            niftic_main = {
+                description = "Main niftic daily backup";
+                repo = "niftic-ssh:/path/to/your/remote/repo";
+                archiveFormat = "{hostname}-{now}";
+                compression = "zlib";
+                paths = [
+                    "/mnt/Data/Backups/Minecraft"
+                    "/mnt/Data/Nextcloud"
+                ];
+                exclude = [
+                    "*.tmp"
+                ];
+                prune = {
+                    keepDaily = 7;
+                    keepWeekly = 4;
+                    keepMonthly = 6;
+                };
+                timer = {
+                    OnCalendar = "daily";
+                    Persistent = true;
+                };
+            };
+
+            azom_main = {
+                description = "Main azom daily backup";
+                repo = "azom-ssh:/data";
                 archiveFormat = "{hostname}-{now}";
                 compression = "zlib";
                 paths = [
